2a9202e5cfb5e86135d824da107ace3dc16c5b5a
cnqlgdl <48677340+cnqlgdl@users.noreply.github.com>
2019-09-22 17:23:14 +1000

    Add files via upload

---
+++ Gps/GeoPoint.java
@@ -0,0 +1,11 @@
+package com.example.mobile_pj2.Data.Model;
+
+public class GeoPoint {
+    public double x;
+    public double y;
+
+    public GeoPoint(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
